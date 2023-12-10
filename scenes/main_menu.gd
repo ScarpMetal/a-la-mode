f875943a1d951a3fe886c3f0644d92d961cb0741
itsjustbrian <bri.ferch@gmail.com>
2023-12-10 14:33:50 -0500

    change type enforcement

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,11 +1,13 @@
 extends Control
 
-func _on_play_pressed():
+
+func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/plating_screen.tscn")
 
-func _on_quit_pressed():
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_credits_pressed():
+func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
