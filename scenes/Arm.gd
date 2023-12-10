d191d2f77387a621adbcb014dbea73e7b1b3d377
itsjustbrian <bri.ferch@gmail.com>
2023-12-10 17:44:40 -0500

    Make vars configureable

--- scenes/Arm.gd
+++ scenes/Arm.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 @export var max_speed: int = 2000
 @export var slow_at_dist: float = 200
-
-var maxY := 1080
-var minY := 400
+@export var max_physics_y := 1080
+@export var min_physics_y := 400
+@export var min_scale := 0.2
+@export var max_scale := 1.0
 
 
 func _physics_process(_delta: float) -> void:
@@ -20,8 +21,9 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity = Vector2(0, 0)
 
-	var mouse_y := get_viewport().get_mouse_position().y
-	var scale_factor := map_value(mouse_y, minY, maxY, 0.2, 1.0)
+	var scale_factor := map_value(
+		mouse_position.y, min_physics_y, max_physics_y, min_scale, max_scale
+	)
 	scale = Vector2(scale_factor, scale_factor)
 
 	move_and_slide()
