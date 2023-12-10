3a434586cf290aa22eeb16a0a8b931f402bd3a72
itsjustbrian <bri.ferch@gmail.com>
2023-12-10 17:38:13 -0500

    Add ice scream zooming

--- scenes/Arm.gd
+++ scenes/Arm.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 @export var max_speed: int = 2000
 @export var slow_at_dist: float = 200
 
+var maxY := 1080
+var minY := 400
+
 
 func _physics_process(_delta: float) -> void:
 	var mouse_position := get_viewport().get_mouse_position()
 	var raw_direction := mouse_position - position
 	var direction := raw_direction.normalized()
 	var dist := raw_direction.length()
+
 	if dist > 0:
 		var unclamped_speed := lerpf(0, max_speed, dist / slow_at_dist)
 		var speed := clampf(unclamped_speed, 0, max_speed)
@@ -16,4 +20,15 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity = Vector2(0, 0)
 
+	var mouse_y := get_viewport().get_mouse_position().y
+	var scale_factor := map_value(mouse_y, minY, maxY, 0.2, 1.0)
+	scale = Vector2(scale_factor, scale_factor)
+
 	move_and_slide()
+
+
+func map_value(
+	value: float, from_min: float, from_max: float, to_min: float, to_max: float
+) -> float:
+	var clamped_value := clampf(value, from_min, from_max)
+	return to_min + (to_max - to_min) * ((clamped_value - from_min) / (from_max - from_min))
