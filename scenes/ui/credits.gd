b7598aeb8035e654efee514b0427b107047a3947
itsjustbrian <bri.ferch@gmail.com>
2023-12-11 03:08:17 -0500

    fix ui path

--- scenes/ui/credits.gd
+++ scenes/ui/credits.gd
@@ -1,12 +1,14 @@
 extends Control
 
+
 func _on_back_pressed() -> void:
 	back()
-	
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action("back"):
 		back()
-		
-func back() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
+
+func back() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
