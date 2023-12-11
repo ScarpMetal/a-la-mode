70eeccfd45a7f9427b2b74ddbfd20a10874c9adc
Matt G <mpgraham917@gmail.com>
2023-12-10 16:26:49 -0800

    Add esc to go back on credits screen

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -1,4 +1,12 @@
 extends Control
 
 func _on_back_pressed() -> void:
+	back()
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action("back"):
+		back()
+		
+func back() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
