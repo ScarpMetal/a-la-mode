b8a6b3ec15b0766089033106ac09096cdf5a48f8
Matt G <mpgraham917@gmail.com>
2023-12-10 16:24:19 -0800

    Add pause screen

---
+++ scenes/pause_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var game_manager : GameManager
+
+func _ready() -> void:
+	print("ready")
+	hide()
+	game_manager.connect("toggle_game_paused", _on_plating_screen_toggle_game_paused)
+
+func _on_plating_screen_toggle_game_paused(is_paused: bool) -> void:
+	if is_paused:
+		show()
+	else:
+		hide()
+
+func _on_resume_pressed() -> void:
+	game_manager.game_paused = false
+	pass
+
+func _on_main_menu_pressed() -> void:
+	game_manager.game_paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
