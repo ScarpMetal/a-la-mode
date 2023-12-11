4503ba433551bdfad72a2b5a9323f2806ff9152b
itsjustbrian <bri.ferch@gmail.com>
2023-12-11 00:08:15 -0500

    Create order manager

---
+++ scenes/OrderManager.gd
@@ -0,0 +1,80 @@
+extends Node
+
+@export var dish_names: Array[String] = ["steak", "veggies", "fish"]
+@export var ice_cream_flavor_names: Array[String] = ["vanilla", "chocolate", "strawberry"]
+@export var time_to_first_order_sec: int = 3
+
+const DIFFICULTY_INTERVALS_SEC: Array[int] = [10, 60, 600]
+
+var time_since_start := 0.0
+var difficulty_level := 0
+var order_timer := Timer.new()
+
+signal order_created(dish_name: String, flavors: Array[String])
+
+
+func _ready() -> void:
+	add_child(order_timer)
+	order_timer.connect("timeout", _on_order_timeout)
+
+
+func _process(delta: float) -> void:
+	if order_timer.paused or order_timer.is_stopped():
+		return
+
+	time_since_start += delta
+	update_difficulty_level()
+
+
+func start() -> void:
+	time_since_start = 0.0
+	difficulty_level = 0
+	order_timer.start(time_to_first_order_sec)
+
+
+func pause() -> void:
+	order_timer.paused = true
+
+
+func resume() -> void:
+	order_timer.paused = false
+
+
+func stop() -> void:
+	order_timer.stop()
+
+
+func update_difficulty_level() -> void:
+	for i in range(len(DIFFICULTY_INTERVALS_SEC)):
+		if time_since_start < DIFFICULTY_INTERVALS_SEC[i]:
+			difficulty_level = i
+			break
+
+
+func _on_order_timeout() -> void:
+	generate_order()
+	order_timer.start(get_time_to_next_order())
+
+
+func get_time_to_next_order() -> int:
+	match difficulty_level:
+		0:
+			return randi_range(4, 6)
+		1:
+			return randi_range(3, 5)
+		2:
+			return randi_range(2, 4)
+		3:
+			return randi_range(1, 3)
+	return 1
+
+
+func generate_order() -> void:
+	var dish: String = dish_names[randi_range(0, dish_names.size() - 1)]
+	var max_flavors: int = difficulty_level + 1
+	var flavors: Array[String] = []
+	for i in randi_range(1, max_flavors):
+		flavors.append(ice_cream_flavor_names[randi_range(0, ice_cream_flavor_names.size() - 1)])
+
+	print("created order: ", dish, flavors)
+	emit_signal("order_created", dish, flavors)
--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -2,7 +2,14 @@ extends Node
 
 class_name GameManager
 
+@onready var order_manager: Node = $OrderManager
+
 signal toggle_game_paused(is_paused: bool)
+
+
+func _ready() -> void:
+	order_manager.start()
+
 
 var game_paused: bool = false:
 	get:
@@ -10,8 +17,10 @@ var game_paused: bool = false:
 	set(value):
 		game_paused = value
 		get_tree().paused = game_paused
+		order_manager.pause() if game_paused else order_manager.resume()
 		emit_signal("toggle_game_paused", game_paused)
-		
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		game_paused = !game_paused
