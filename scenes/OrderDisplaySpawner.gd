84020595def8066ab73c4f687fc1ee2205c18200
Brian Ferch <bri.ferch@gmail.com>
2023-12-26 02:18:15 -0500

    free order displays after removal

--- scenes/OrderDisplaySpawner.gd
+++ scenes/OrderDisplaySpawner.gd
@@ -18,27 +18,35 @@ func remove_custom(method: Callable, arr: Array[Variant]) -> Variant:
 			return arr.pop_at(index)
 	return null
 
-func all_signals(coroutines: Array[Signal]) -> Array[Variant]:
+func all_promises(promises: Array[Promise]) -> Array[Variant]:
 	var result_values: Array[Variant] = []
-	for coroutine in coroutines:
-		result_values.append(await coroutine)
+	for promise in promises:
+		result_values.append(await promise.completed)
 	return result_values
 
 class Promise extends RefCounted:
 	signal _completed_signal(result: Variant)
-	var _completed_value: Variant = null
+	var _resolved: bool = false
+	var _result_value: Variant = null
 
 	var completed: Variant:
 		get:
-			if _completed_value != null:
-				return _completed_value
-			else:
-				return _completed_signal
+			return _completed_signal
+			# if not _resolved:
+			# 	return _completed_signal
+			# else:
+			# return _result_value
 
-	func _init(async_func: Callable) -> void:
-		var result: Variant = await async_func.call()
-		_completed_signal.emit(result)
+	func _init(init: Variant) -> void:
+		if init is Callable:
+			_completed_signal.connect(_on_completed)
+			init.call(_completed_signal)
+		elif init is Signal:
+			init.connect(_on_completed)
 
+	func _on_completed(result: Variant) -> void:
+		_result_value = result
+		_resolved = true
 
 func process_operation_queue() -> void:
 	if len(operation_queue) == 0:
@@ -49,7 +57,7 @@ func process_operation_queue() -> void:
 	var operation_order_container: OrderContainer = operation_queue_item.get("order_container")
 
 	if operation == "remove":
-		var tween_signals: Array[Signal] = []
+		var tween_promises: Array[Promise] = []
 		var order_container_index := find_custom(
 			func(order_container: OrderContainer) -> bool: 
 				return order_container.order.id == operation_order_container.order.id,
@@ -59,64 +67,58 @@ func process_operation_queue() -> void:
 			if len(active_order_containers) > max_orders_displayed:
 				for i in range(order_container_index + 1, min(max_orders_displayed, len(active_order_containers))):
 					(
-						tween_signals.append(create_tween()
+						tween_promises.append(Promise.new(create_tween()
 						. tween_property(active_order_containers[i], "position", Vector2(active_order_containers[i].position.x - 250, 0), 1)
 						. set_trans(Tween.TRANS_SPRING)
-						. finished)
+						. finished))
 					)
 
 			if len(active_order_containers) > max_orders_displayed:
 				var next_order_container: OrderContainer = active_order_containers[max_orders_displayed]
-				tween_signals.append(create_tween()
+				tween_promises.append(Promise.new(create_tween()
 					.tween_property(next_order_container, "position", Vector2.ZERO, 1)
 					.set_trans(Tween.TRANS_SPRING)
-					.finished)
+					.finished))
 
 			var order_container: OrderContainer = active_order_containers.pop_at(order_container_index)
-			# tween_signals.append(create_tween()
-			# . tween_property(
-			# 	order_container,
-			# 	"position",
-			# 	Vector2(order_container.position.x, -500), 1
-			# )
-			# . set_trans(Tween.TRANS_SPRING)
-			# . finished)
 
-			var tween_and_free := func() -> void:
+			var tween_and_free := func(completed_signal: Signal) -> void:
 				(
 					await create_tween()
 					. tween_property(
 						order_container,
 						"position",
-						Vector2(0, order_container.position.y - 500), 1
+						Vector2(order_container.position.x, -500), 1
 					)
 					. set_trans(Tween.TRANS_SPRING)
 					. finished
 				)
+				print("freeing")
 				order_container.queue_free()
+				completed_signal.emit()
 
-			var promise := Promise.new(tween_and_free)
+			var promise: Promise = Promise.new(tween_and_free)
+			tween_promises.append(promise)
 
-			# tween_signals.append(tween_and_free.)
+		await all_promises(tween_promises)
 
-		await all_signals(tween_signals)
 
 	if operation == "add":
-		var tween_signals: Array[Signal] = []
+		var tween_promises: Array[Promise] = []
 		if len(active_order_containers) < max_orders_displayed:
 			for order_container in active_order_containers:
-				tween_signals.append(create_tween()
+				tween_promises.append(Promise.new(create_tween()
 					. tween_property(order_container, "position", Vector2(order_container.position.x - 250, 0), 1)
 					. set_trans(Tween.TRANS_SPRING)
-					. finished)
+					. finished))
 
-			tween_signals.append(create_tween()
+			tween_promises.append(Promise.new(create_tween()
 				.tween_property(operation_order_container, "position", Vector2.ZERO, 1)
 				.set_trans(Tween.TRANS_SPRING)
-				.finished)
+				.finished))
 
 		active_order_containers.append(operation_order_container)
-		await all_signals(tween_signals)
+		await all_promises(tween_promises)
 
 	process_operation_queue()
 
